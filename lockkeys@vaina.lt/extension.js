36375664ed3f7f4a9cdd382f7c5e608c80198fb3
bywkgexmkiltxh <ascdasvcfasx@outlook.com>
2025-10-20 20:42:55 +0300

    Add dynamic highContrast watching

--- lockkeys@vaina.lt/extension.js
+++ lockkeys@vaina.lt/extension.js
@@ -82,14 +82,14 @@ const LockKeysIndicator = GObject.registerClass({
         this.capsMenuItem = new PopupMenu.PopupSwitchMenuItem(_("Caps Lock"), false, { reactive: false });
         this.menu.addMenuItem(this.capsMenuItem);
 
-        const a11ySettings = new Gio.Settings({
+        this.a11ySettings = new Gio.Settings({
             schema: 'org.gnome.desktop.a11y.interface'
         });
-        const isHighContrast = a11ySettings.get_boolean('high-contrast');
-        const disabledOpacity = isHighContrast ? 0.4 : 0.5;
 
-        this.numMenuItem._switch.set_opacity(disabledOpacity * 255);
-        this.capsMenuItem._switch.set_opacity(disabledOpacity * 255);
+        this.updateSwitchOpacity();
+        this._highContrastChangedId = this.a11ySettings.connect('changed::high-contrast', () => {
+            this.updateSwitchOpacity();
+        });
 
         this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
         this.settingsMenuItem = new PopupMenu.PopupMenuItem(_("Settings"));
@@ -100,6 +100,14 @@ const LockKeysIndicator = GObject.registerClass({
 
         this.indicatorStyle = new HighlightIndicatorStyle(this);
 		this.updateState();
+    }
+
+    updateSwitchOpacity() {
+        const isHighContrast = this.a11ySettings.get_boolean('high-contrast');
+        const disabledOpacity = isHighContrast ? 0.4 : 0.5;
+        
+        this.numMenuItem._switch.set_opacity(disabledOpacity * 255);
+        this.capsMenuItem._switch.set_opacity(disabledOpacity * 255);
     }
 
 	setActive(enabled) {
@@ -115,6 +123,10 @@ const LockKeysIndicator = GObject.registerClass({
 			this._settingsChangedId = 0;
             this.icons.iconTheme.disconnect(this._iconThemeChangedId);
 			this._iconThemeChangedId = 0;
+            if (this._highContrastChangedId) {
+                this.a11ySettings.disconnect(this._highContrastChangedId);
+                this._highContrastChangedId = 0;
+            }
 		}
 	}
 
